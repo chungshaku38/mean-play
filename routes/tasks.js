ac83e6fcdb5b15b59a8863bf72e08a568af8d5a8
chungnguyen <chungshaku38@gmail.com>
2018-03-19 15:16:46 +0700

    add common method to allow create/update/delete

--- routes/tasks.js
+++ routes/tasks.js
@@ -16,7 +16,7 @@ router.get('/tasks', function(req, res, next) {
 });
 
 //Get single task
-router.get('/tasks/:id', function(req, res, next) {
+router.get('/task/:id', function(req, res, next) {
     db.tasks.findOne({_id: mongojs.ObjectId(req.params.id)}, function(err, task) {
         if(err) {
             res.send(err);
@@ -26,6 +26,65 @@ router.get('/tasks/:id', function(req, res, next) {
     });
 });
 
+//Save task
+router.post('/task', function(req, res, next) {
+    var task = req.body;
+    if(!task.title || !(task.isDone + '')) {
+        res.status(400);
+        res.json({
+            "error" : "bad data"
+        })
+    } else {
+        db.tasks.save(task, function(err, task) {
+            if(err) {
+                res.send(err);
+            } else {
+                res.json(task);
+            }
+        });
+    }
+});
+
+//Delete task
+router.delete('/task/:id', function(req, res, next) {
+    db.tasks.remove({_id: mongojs.ObjectId(req.params.id)}, function(err, task) {
+        if(err) {
+            res.send(err);
+        } else {
+            res.json(task);
+        }
+    });
+});
+
+//Update task
+router.put('/task/:id', function(req, res, next) {
+    var task = req.body;
+    var updTask = {};
+    //some sample
+    if(task.isDone) {
+        updTask.isDone = task.isDone;
+    }
+
+    if(task.title) {
+        updTask.title = task.title;
+    }
+    
+    if(!updTask) {
+        res.status(400);
+        res.json({
+            "error": "bad data"
+        });
+    } else {
+        //third params is option, specify custom behavior
+        db.tasks.update({_id: mongojs.ObjectId(req.params.id)}, updTask, function(err, task) {
+            if(err) {
+                res.send(err);
+            } else {
+                res.json(task);
+            }
+        });
+    }
+});
 
 //export to access from other files
 module.exports = router;
